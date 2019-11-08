68c1c77a46258db2a7cc8d8829980648d1248918
Tortilla123 <jp568@njit.edu>
2019-11-07 21:27:36 -0600

    Updates

---
+++ precache-manifest.6fbb933c5765c8c915ac05a88598d5df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7cb3eb10bae8bfddb654d8e8d25abd",
+    "url": "/testportfolio/index.html"
+  },
+  {
+    "revision": "129e1a317a2b2d3d4f2b",
+    "url": "/testportfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3990fe6834f2a273cf34",
+    "url": "/testportfolio/static/js/2.20ad84a0.chunk.js"
+  },
+  {
+    "revision": "129e1a317a2b2d3d4f2b",
+    "url": "/testportfolio/static/js/main.a81edccf.chunk.js"
+  },
+  {
+    "revision": "711d378318c7245856e0",
+    "url": "/testportfolio/static/js/runtime-main.7b6807c9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/testportfolio/static/media/logo.25bf045c.svg"
+  }
+]);
